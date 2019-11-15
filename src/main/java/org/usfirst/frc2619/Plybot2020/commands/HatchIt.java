13bb203362ce8f444edeb314fa54727cb4b503c5
Min <mingjo20@midlandps.org>
2019-11-14 20:46:02 -0500

    In command, HatchIt, I made a command that calls a subsystem, Hatch, to call a method, hatchPlease, to hatch the piston of the robot.

--- src/main/java/org/usfirst/frc2619/Plybot2020/commands/HatchIt.java
+++ src/main/java/org/usfirst/frc2619/Plybot2020/commands/HatchIt.java
@@ -38,11 +38,13 @@ public class HatchIt extends Command {
     // Called just before this Command runs the first time
     @Override
     protected void initialize() {
+        Robot.hatch.hatchPlease();
     }
 
     // Called repeatedly when this Command is scheduled to run
     @Override
     protected void execute() {
+        
     }
 
     // Make this return true when this Command no longer needs to run execute()
