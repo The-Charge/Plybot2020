b6f7b7707176b7d1e22156c36c86ff1ca68789f1
abcurrie <abcurrie@gmail.com>
2019-11-21 19:55:30 -0500

    Made Intake subsystem with SpeedMode and PowerMode thing in it

--- src/main/java/org/usfirst/frc2619/Plybot2020/commands/HatchIt.java
+++ src/main/java/org/usfirst/frc2619/Plybot2020/commands/HatchIt.java
@@ -38,6 +38,7 @@ public class HatchIt extends Command {
     // Called just before this Command runs the first time
     @Override
     protected void initialize() {
+        Robot.hatch.HatchIt();
     }
 
     // Called repeatedly when this Command is scheduled to run
